777adb88504362111e59a3239644b4497b19888a
ngeno7 <hillaryngeno7@gmail.com>
2023-12-14 15:16:32 +0300

    add updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsComponent } from './forms/forms.component';
+import { TablesComponent } from './tables/tables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tables', component: TablesComponent,
+  },
+  {
+    path: 'profile', component: ProfileComponent,
+  },
+  {
+    path: 'forms', component: FormsComponent,
+  },
+  {
+    path: '', component: DashboardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TablesComponent } from './tables/tables.component';
+import { FormsComponent } from './forms/forms.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TablesComponent,
+    FormsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
